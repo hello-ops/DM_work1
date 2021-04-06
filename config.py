c2b4b0343c9cbc7735a56366169b20cb24ca96f0
hello-ops <56348742+hello-ops@users.noreply.github.com>
2021-04-06 09:56:37 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-dataset1_path = "wine-reviews"
-dataset1_list = ["winemag-data_first150k.csv","winemag-data-130k-v2.csv"]
-
-dataset2_path = "Chicago Building Violations"
-dataset2_list = ["building-violations.csv"]
